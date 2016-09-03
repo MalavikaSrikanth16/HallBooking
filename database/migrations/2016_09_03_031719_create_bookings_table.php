3c1fb7ffb15f6fee6b51e89107897f0d49676a11
Chandana02 <sshchandana.bitra@gmail.com>
2016-09-03 09:30:42 +0530

    Add migrations
    Created migrations for slots table, halls table, booking table and
    slots_booked table

---
+++ database/migrations/2016_09_03_031719_create_bookings_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBookingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('booking', function (Blueprint $table) {
+            $table->increments('id');
+            $table->date('date');
+            $table->integer('hall_booked');
+            $table->string('prof_name');
+            $table->string('webmail_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('booking');
+    }
+}
