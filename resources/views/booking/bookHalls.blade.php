d553aef6dbeb2ece4deea9ba2e6dae3132e13edc
MalavikaSrikanth16 <malu.srikanth@gmail.com>
2016-10-01 10:42:37 +0530

    color added

--- resources/views/booking/bookHalls.blade.php
+++ resources/views/booking/bookHalls.blade.php
@@ -3,7 +3,27 @@
 @section('content')
 
 	{!! Form::open(array('url' => 'bookHalls', 'method' => 'POST')) !!}
+<style>
+#adminbooked{
+	background-color: red;
+	width: 15px;
+	height:15px;
+	margin-top: 13px;
+	margin-bottom: 10px;
+	margin-left: 10px;
+	border-radius:3px;
+}
+#facultybooked{
+	background-color: yellow;
+	width: 15px;
+	height:15px;
+	margin-top: 13px;
+	margin-bottom: 10px;
+	margin-left: 10px;
+	border-radius:3px;
 
+}
+</style>
 		<div class="container">
 
 			<div class="form-group">
@@ -22,13 +42,14 @@
 			    						<ul class="list-inline">
 			    							@for($i=0,$j=0,$k=0;$i<count($times[$key]);$i++)
 			    								@if($j<count($adminBookedSlots[$key]) && $i==($adminBookedSlots[$key][$j]-1))
+		    										
 		    										<li class="list-group-item" > 
-									                    <input class="form-control" type="checkbox" value="{{ ($key).','.($times[$key][$i]) }}" style="background-color: yellow;" disabled checked>
+									                    <div id="adminbooked"></div>
 									                    {{ $times[$key][$i] }} 
 									                    <?php $j++; ?>
 				    							@elseif($k<count($facultyBookedSlots[$key]) && $i==($facultyBookedSlots[$key][$k]-1))
 		    										<li class="list-group-item" > 
-									                    <input class="form-control" type="checkbox" disabled value="{{ ($key).','.($times[$key][$i]) }}" style="background-color: red;" disabled checked>
+									                    <div id="facultybooked"></div>
 									                    {{ $times[$key][$i] }} 
 									                    <?php $k++; ?>
 				    							@else
